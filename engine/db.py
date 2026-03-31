c8c6b022b04c34ae38b21d07bf0aec7f53107414
shuvo3954-design <shovo3954@gamil.com>
2026-03-31 15:37:39 +0600

    creating databse and stored url and link

---
+++ engine/db.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+con = sqlite3.connect("mind_game.db")
+cursor = con.cursor()
+
+
+# 2. Create the table with 'UNIQUE' on the name column
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS sys_command(
+    id INTEGER PRIMARY KEY, 
+    name VARCHAR(50) UNIQUE, 
+    path VARCHAR(1000)
+)""")
+
+# # 3. Insert the data safely
+# query = "INSERT OR IGNORE INTO sys_command (name, path) VALUES (?, ?)"
+# cursor.execute(query, ('ONENOTE', "C:\\Program Files\\Microsoft Office\\root\Office16\\ONENOTE.EXE"))
+
+# # 4. Save and close
+# con.commit()
+# con.close()
+
+query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+cursor.execute(query)
+
+query = "INSERT INTO web_command VALUES (null,'Gemini Ai', 'https://gemini.google.com/app')"
+cursor.execute(query)
+con.commit()
+
+query = "INSERT INTO web_command VALUES (null,'Claude', 'https://claude.ai/new')"
+cursor.execute(query)
+con.commit()
