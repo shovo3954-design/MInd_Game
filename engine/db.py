655abacbcad1cedaa2ec798a027465ef65365244
shuvo3954-design <shovo3954@gamil.com>
2026-04-04 12:02:07 +0600

    adding new contract database

--- engine/db.py
+++ engine/db.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 
 con = sqlite3.connect("mind_game.db")
@@ -5,12 +6,15 @@ cursor = con.cursor()
 
 
 # 2. Create the table with 'UNIQUE' on the name column
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS sys_command(
-    id INTEGER PRIMARY KEY, 
-    name VARCHAR(50) UNIQUE, 
-    path VARCHAR(1000)
-)""")
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS sys_command(
+#     id INTEGER PRIMARY KEY, 
+#     name VARCHAR(50) UNIQUE, 
+#     path VARCHAR(1000)
+# )""")
+
+
+
 
 # # 3. Insert the data safely
 # query = "INSERT OR IGNORE INTO sys_command (name, path) VALUES (?, ?)"
@@ -20,10 +24,51 @@ CREATE TABLE IF NOT EXISTS sys_command(
 # con.commit()
 # con.close()
 
-query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
-cursor.execute(query)
+
+
+# query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+# cursor.execute(query)
+
+
 
 # query = "INSERT INTO web_command VALUES (null,'Gemini Ai', 'https://gemini.google.com/app')"
 # cursor.execute(query)
 # con.commit()
 
+
+
+# Create a table with the desired columns
+# cursor.execute(
+#     '''CREATE TABLE IF NOT EXISTS contacts 
+#     (id integer primary key, 
+#     name VARCHAR(200), 
+#     mobile_no VARCHAR(255), 
+#     email VARCHAR(255) NULL)'''
+#     )
+
+
+
+
+
+# Specify the column indices you want to import (0-based index)
+# Example: Importing the 1st and 3rd columns
+# desired_columns_indices = [0, 20]
+
+# # Read data from CSV and insert into SQLite table for the desired columns
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+# # Commit changes and close connection
+# con.commit()
+# con.close()
+
+
+
+
+#### 4. Insert Single contacts (Optional)
+query = "INSERT INTO contacts VALUES (null,'helal', '1234567890', 'null')"
+cursor.execute(query)
+con.commit()
